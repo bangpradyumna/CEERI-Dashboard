c9dcdb8873e0011c9ac5a8255f8b1ad5a2c72cc1
Darsh Patel <darshpatel59m@gmail.com>
2020-01-29 18:14:35 +0530

    initMap is working now

--- js/map.js
+++ js/map.js
@@ -1,16 +1,27 @@
 var url = "https://darshmap.free.beeceptor.com";
 
-var props = getProps(url);
-var map = mapInit(props.center);
+// var props = getProps(url);
+// console.log(props);
 
-function getProps(url) 
-{
-  let response = fetch(url);
-  let data = response.json();
-  return data;
-}
+let map = fetch(url)
+            .then(response => response.json())
+            .then(data => mapInit(data.center));
+
+// var props;
+// fetch(url)
+//     .then(response => response.json())
+//     .then(data => mapInit(data.center));
+// var map = mapInit(props.center);
+// 
+// function getProps(url) 
+// {
+//   let response = fetch(url);
+//   let data = response.json();
+//   return response;
+// }
 
 function mapInit(center) {
     var tempMap = new MapmyIndia.Map("map",{ center:[center.lat, center.lng],zoomControl: true,hybrid:true,zoom:4 });
     return tempMap;
 }
+
