a6da542329ca55ea9f696d3275d7437258f6ab3c
Pradyumna <f20180290@pilani.bits-pilani.ac.in>
2020-01-29 21:01:58 +0530

    Completed javascript functions for map.

--- js/map.js
+++ js/map.js
@@ -1,27 +1,51 @@
-var url = "https://darshmap.free.beeceptor.com";
+let url = "http://www.mocky.io/v2/5e3186623200002b008885ee";
+var map;
 
-// var props = getProps(url);
-// console.log(props);
-
-let map = fetch(url)
-            .then(response => response.json())
-            .then(data => mapInit(data.center));
-
-// var props;
-// fetch(url)
-//     .then(response => response.json())
-//     .then(data => mapInit(data.center));
-// var map = mapInit(props.center);
-// 
-// function getProps(url) 
-// {
-//   let response = fetch(url);
-//   let data = response.json();
-//   return response;
-// }
-
-function mapInit(center) {
-    var tempMap = new MapmyIndia.Map("map",{ center:[center.lat, center.lng],zoomControl: true,hybrid:true,zoom:4 });
-    return tempMap;
+window.onload = function loadPage(){
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            mapInit(data.center); // Initialize map using center coordinates
+            // addDefaultData(data.def);
+            fetchDataForNode(data.def);
+            return data.markers;
+        })
+        .then((data) =>{
+            addAllMarkers(data);
+        });
 }
 
+function mapInit(center) {
+    map = new MapmyIndia.Map("map",{ center:[center.lat, center.lng],zoomControl: true,hybrid:true,zoom:4 });
+}
+
+function addAllMarkers(markers){
+    markers.forEach(element => {
+        addMarker(element);
+    });
+}
+
+function addMarker(mk){
+    let tempMk = new L.marker([mk.lat, mk.lng]).addTo(map);
+    tempMk.on("click", function (e) {
+        console.log(mk);
+        fetchDataForNode(mk);
+    })
+    return tempMk;
+}
+
+// function addDefaultData(props){
+//     // Something to fetch data of default city
+//     return;
+// }
+
+function fetchDataForNode(props){
+    // Fetch a data for node "props"
+    // let property_div = document.getElementById("pm2_5");
+    // property_div.innerHTML = "20,20,20,20,20,20,20";
+    $('.sparkbar.pm10').sparkline([1,5,-1,-10,20], { type: 'bar', height: '20', barWidth: 40, barSpacing: 4, barColor: '#00C0DD', negBarColor: "#001DFF" });
+    // console.log("Fetching data for ");
+    // console.log(props);
+}
